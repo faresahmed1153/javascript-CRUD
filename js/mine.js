49043afbd9f82fb36ff218d1ffbf0096527fa08a
fares <faresahmed3090@gmail.com>
2021-12-23 18:44:17 +0200

    final

---
+++ js/mine.js
@@ -0,0 +1,103 @@
+var products = [];
+
+var productName = document.getElementById("productName");
+var productCategory = document.getElementById("productCategory");
+var productPrice = document.getElementById("productPrice");
+var productDescription = document.getElementById("productDescription");
+
+function createProduct() {
+  var product = {
+    pName: productName.value,
+    pCategory: productCategory.value,
+    pPrice: productPrice.value,
+    pDescription: productDescription.value,
+  };
+
+  products.push(product);
+  console.log(products);
+
+  displayProduct();
+  clearForm();
+}
+
+function displayProduct() {
+  var holder = "";
+
+  for (var i = 0; i < products.length; i++) {
+    holder += `<tr>
+    <td scope="row">${i}</td>
+                <td scope="row">${products[i].pName}</td>
+            <td scope="row">${products[i].pCategory}</td>
+            <td scope="row">${products[i].pPrice}</td>
+            <td scope="row">${products[i].pDescription}</td>
+            <td scope="row"><button onclick="updateProduct1(${i});"  class="btn btn-warning">
+              <i class="fas fa-edit"></i></button>
+            </td>
+            <td scope="row"><button onclick="deleteProduct(${i});"  class="btn btn-danger">
+              <i class="fas fa-trash"></i></button>
+            </td>
+          </tr>`;
+  }
+  document.getElementById("tbody").innerHTML = holder;
+}
+
+function clearForm() {
+  productName.value = "";
+  productCategory.value = "";
+  productPrice.value = "";
+  productDescription.value = "";
+}
+
+function deleteProduct(index) {
+  products.splice(index, 1);
+  displayProduct();
+}
+
+function updateProduct1(index) {
+  document.getElementById("create").style.display = "none";
+  productName.value = products[index].pName;
+  productCategory.value = products[index].pCategory;
+  productPrice.value = products[index].pPrice;
+  productDescription.value = products[index].pDescription;
+  document.getElementById("update").style.display = "inline-block";
+  document.getElementById("update").value = index;
+}
+function updateProduct2() {
+  index = document.getElementById("update").value;
+  products[index].pName = productName.value;
+  products[index].pCategory = productCategory.value;
+  products[index].pPrice = productPrice.value;
+  products[index].pDescription = productDescription.value;
+
+  document.getElementById("create").style.display = "inline-block";
+  document.getElementById("update").style.display = "none";
+  displayProduct();
+  clearForm();
+}
+
+function searchProduct() {
+  var searchInput = document.getElementById("searchInput").value;
+  var holder = "";
+
+  for (var i = 0; i < products.length; i++) {
+    if (
+      products[i].pName.toUpperCase().includes(searchInput.toUpperCase()) ==
+      true
+    ) {
+      holder += `<tr>
+    <td scope="row">${i}</td>
+                <td scope="row">${products[i].pName}</td>
+            <td scope="row">${products[i].pCategory}</td>
+            <td scope="row">${products[i].pPrice}</td>
+            <td scope="row">${products[i].pDescription}</td>
+            <td scope="row"><button onclick="updateProduct1(${i});"  class="btn btn-warning">
+              <i class="fas fa-edit"></i></button>
+            </td>
+            <td scope="row"><button onclick="deleteProduct(${i});"  class="btn btn-danger">
+              <i class="fas fa-trash"></i></button>
+            </td>
+          </tr>`;
+    }
+  }
+  document.getElementById("tbody").innerHTML = holder;
+}
